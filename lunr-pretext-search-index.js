76013f10a3da827aa9006e1204b607eb30f43da8
Ian Curtis <icurt523@gmail.com>
2023-09-07 11:01:50 -0400

    Latest build deployed.

---
+++ lunr-pretext-search-index.js
@@ -0,0 +1,166 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "colophon-1",
+  "level": "1",
+  "url": "colophon-1.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": " copyright  "
+},
+{
+  "id": "preface-1",
+  "level": "1",
+  "url": "preface-1.html",
+  "type": "Preface",
+  "number": "",
+  "title": "Materials",
+  "body": " Materials  The materials required for this book are just you! Read on and learn about dogs!  "
+},
+{
+  "id": "S_toy_group",
+  "level": "1",
+  "url": "S_toy_group.html",
+  "type": "Section",
+  "number": "1.1",
+  "title": "The Toy Group",
+  "body": " The Toy Group  So you like tiny dogs, huh? You've come to the right place to learn about them!  "
+},
+{
+  "id": "S_sporting_group",
+  "level": "1",
+  "url": "S_sporting_group.html",
+  "type": "Section",
+  "number": "1.2",
+  "title": "Sporting Group",
+  "body": " Sporting Group   Walk-Through Examples in <p>  For instance, you can type regular text! Yay sporting group dogs! 23! é!  This is math! Yay! Next, let's look at displayed math!      In R, there are multiple packages that can be used such as ggplot2 and rmarkdown . Moreover, you can install and import packages super quick!  install.packages(\"ggplot2\")  library(ggplot2)      Pre-Made Examples in <p>  If you want a quote, use the <q> tag: Sporting group dogs are Naturally active and alert and make likeable, well-rounded companions (American Kennel Club). Blockquotes work too!   Dogs are good friends.  Unknown   You can also make lists:   Spaniels    Pointers    Retrievers    Setters      Find a dog you like    Give it a name    Play with it and love it forever      Curly  Dogs with curly fur have curls in their fur.    Short-Haired  Short-haired dogs will have fur that is not long.     Sometimes, you need to apply emphasis to words. You can't use italics or bold with PreTeXt but you can communicate based on a tone. Sorry, c'est la vie !  Have you ever read Dogs by Lewis Blackwell and Tim Flach? It's all about dogs! If you need something shorter, try Dogs Are Even More Like Us Than We Thought from National Geographic . These resources are great. There are many more resources to read about dogs! You could even watch a documentary!   Remember, this section is all about the Sporting Group. If you want to learn about the Toy Group, try ! In fact, all we have right now are two sections: Toy Group & Sporting Group. We have sections planned for the rest of the groups and will be titled <Group Name> Group.    Pre-Made Examples with Other Elements   My Dog Sadie, a goldendoodle   A tan-colored dog laying in an empty green grass field illuminated by sunshine.     For example, you can create an example!     There are many examples of sporting group dogs. We can describe some.    Labrador Retriever...    Golden Retriever    American Water Spaniel       Dogs are humankind's best friend.    Just ask the majority of history.       Sporting dogs are hunters and are good at retrieving game.     Note on My Dog  It's true that my dog is half sporting group, but would not be good at any typical sporting group activities (such as hunting).    Examples of Sporting Group Dogs    Spaniels  Pointers  Retrievers  Setters    American Water Spaniel  Bracco Italiano  Golden Retriever  English Setter    English Cocker Spaniel  German Shorthaired Pointer  Curly-Coated Retriever  Gordon Setter      Give one example of a sporting group dog.      What were sporting group dogs used for in the past?    Give an example of a dog from each of the following categories.   Spaniels   Use for help!   Pointers   Perhaps you could say the Bracco Italiano or the German Shorthaired Pointer.   Retrievers   Setters    "
+},
+{
+  "id": "fig-sadie",
+  "level": "2",
+  "url": "S_sporting_group.html#fig-sadie",
+  "type": "Figure",
+  "number": "1.2.1",
+  "title": "",
+  "body": " My Dog Sadie, a goldendoodle   A tan-colored dog laying in an empty green grass field illuminated by sunshine.   "
+},
+{
+  "id": "ex-example",
+  "level": "2",
+  "url": "S_sporting_group.html#ex-example",
+  "type": "Example",
+  "number": "1.2.2",
+  "title": "",
+  "body": " For example, you can create an example!  "
+},
+{
+  "id": "ex-example-extended",
+  "level": "2",
+  "url": "S_sporting_group.html#ex-example-extended",
+  "type": "Example",
+  "number": "1.2.3",
+  "title": "",
+  "body": "  There are many examples of sporting group dogs. We can describe some.    Labrador Retriever...    Golden Retriever    American Water Spaniel   "
+},
+{
+  "id": "thm-dogs-cute",
+  "level": "2",
+  "url": "S_sporting_group.html#thm-dogs-cute",
+  "type": "Theorem",
+  "number": "1.2.4",
+  "title": "",
+  "body": "  Dogs are humankind's best friend.    Just ask the majority of history.   "
+},
+{
+  "id": "def-dogs",
+  "level": "2",
+  "url": "S_sporting_group.html#def-dogs",
+  "type": "Definition",
+  "number": "1.2.5",
+  "title": "",
+  "body": "  Sporting dogs are hunters and are good at retrieving game.   "
+},
+{
+  "id": "tbl-sporting",
+  "level": "2",
+  "url": "S_sporting_group.html#tbl-sporting",
+  "type": "Table",
+  "number": "1.2.6",
+  "title": "Examples of Sporting Group Dogs",
+  "body": " Examples of Sporting Group Dogs    Spaniels  Pointers  Retrievers  Setters    American Water Spaniel  Bracco Italiano  Golden Retriever  English Setter    English Cocker Spaniel  German Shorthaired Pointer  Curly-Coated Retriever  Gordon Setter    "
+},
+{
+  "id": "chkpt-sporting",
+  "level": "2",
+  "url": "S_sporting_group.html#chkpt-sporting",
+  "type": "Checkpoint",
+  "number": "1.2.7",
+  "title": "",
+  "body": " Give one example of a sporting group dog.  "
+},
+{
+  "id": "exercise-2",
+  "level": "2",
+  "url": "S_sporting_group.html#exercise-2",
+  "type": "Exercise",
+  "number": "1.2.4.1",
+  "title": "",
+  "body": " What were sporting group dogs used for in the past?  "
+},
+{
+  "id": "exercise-3",
+  "level": "2",
+  "url": "S_sporting_group.html#exercise-3",
+  "type": "Exercise",
+  "number": "1.2.4.2",
+  "title": "",
+  "body": " Give an example of a dog from each of the following categories.   Spaniels   Use for help!   Pointers   Perhaps you could say the Bracco Italiano or the German Shorthaired Pointer.   Retrievers   Setters  "
+},
+{
+  "id": "appendix-1",
+  "level": "1",
+  "url": "appendix-1.html",
+  "type": "Appendix",
+  "number": "A",
+  "title": "Acknowledgements",
+  "body": " Acknowledgements  This book was made possible by the internet. Thanks for teaching my enough about dogs to write a simple example book.  "
+},
+{
+  "id": "app_answers",
+  "level": "1",
+  "url": "app_answers.html",
+  "type": "Appendix",
+  "number": "B",
+  "title": "Answers to All Exercises",
+  "body": " Answers to All Exercises  "
+},
+{
+  "id": "index-1",
+  "level": "1",
+  "url": "index-1.html",
+  "type": "Index",
+  "number": "",
+  "title": "Index",
+  "body": "  "
+},
+{
+  "id": "colophon-2",
+  "level": "1",
+  "url": "colophon-2.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": " This book was authored in PreTeXt .  "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
